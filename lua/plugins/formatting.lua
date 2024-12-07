736412b0edb161f94c914c4d6c8b97d18b7508ff
= <akhilmhdh@gmail.com>
2024-12-07 20:32:29 +0530

    feat: added support for mdx

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -16,6 +16,7 @@ return {
 				json = { "prettierd" },
 				yaml = { "prettierd" },
 				markdown = { "prettierd" },
+				mdx = { "prettierd" },
 				graphql = { "prettierd" },
 				liquid = { "prettierd" },
 				lua = { "stylua" },
